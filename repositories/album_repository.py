1ac41fc0dd73003deabdd2597c700fec07159a48
Evan Reid <evan_1995@hotmail.com>
2020-11-24 16:39:44 +0000

    added select-all function to album_repo

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -4,15 +4,27 @@ from db.run_sql import run_sql
 
 #CREATE
 def save(album):
-    sql = "INSERT INTO album (artist, title, genre) VALUES (%s, %s, %s) RETURNING *"
-    values = [album.artist, album.title, album.genre]
+    sql = "INSERT INTO album (artist, arist_id, title, genre) VALUES (%s, %s, %s) RETURNING *"
+    values = [album.artist, album.artist_id, album.title, album.genre]
     results = run_sql(sql, values)
     id = results[0]['id']
     album.id = id
     return album
 
 #READ - select all
+
+def select_all():
+    albums = []
+    sql = "SELECT * FROM albums"
+    results = run_sql(sql)
+    for row in results:
+        # arist = artist_repository.select(row['artist_id'])
+        album = Album(row['artist'], row['title'], row['genre'], row['id'])
+        albums.append(album)
+    return albums
+
 #READ - select one by id
+
 #DELETE - all
 #DELETE - one
 #UPDATE
